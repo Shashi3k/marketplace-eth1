79f43e3f63651aec8f8bfd6017f79b951a42c5fd
Shashi <shashi3k3k@gmail.com>
2023-08-12 15:05:22 +0530

    commiting before starting the day

--- components/providers/web3/index.js
+++ components/providers/web3/index.js
@@ -29,7 +29,8 @@ export default function Web3Provider({children}){
 
             if (provider){
                 const web3=new Web3(provider)
-                const contract=loadContract("CourseMarketPlace",provider)
+                const contract=loadContract("CourseMarketPlace",web3)
+                console.log(contract)
                 setWeb3Api(
                     {web3,
                     provider,
